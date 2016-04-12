7f52ad0b052e4f10aab905dafde4f9c93f1e8ba8
tjaydkSchool <dennis.michael@jubii.dk>
2016-04-12 11:32:36 +0200

    Hello

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ball.transform.position.y < -2) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+		}
+	}
+}
